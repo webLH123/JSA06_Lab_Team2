ae4dfd4621202a13af6cdb9ecb22ea8712002159
GiangNN20XX <jacknguyen12111996@gmail.com>
2022-12-04 10:42:52 +0700

    Lab 2

--- Lab 2/Nam Giang/main.js
+++ Lab 2/Nam Giang/main.js
@@ -3,11 +3,11 @@ document.getElementById("demo").innerHTML = fruits[0]
 var morefruits = [["Grape",'15'],['Apple','15'],['Banana','15']]
 document.getElementById("demo").innerHTML = morefruits
 console.log("I have 15 grapes")
-var myDog = {
+let myDog = {
     "name": "Ngáo",
     "legs": 4,
     "friends": ["everything!"]
 };
-console.log(myDog)
+
 
 
