afd6c8de8770dbb40ed071e4606bb9819588d5ba
GiangNN20XX <jacknguyen12111996@gmail.com>
2022-12-04 11:04:51 +0700

    Lab 2

--- Lab 2/Nam Giang/main.js
+++ Lab 2/Nam Giang/main.js
@@ -3,11 +3,13 @@ document.getElementById("demo").innerHTML = fruits[0]
 var morefruits = [["Grape",'15'],['Apple','15'],['Banana','15']]
 document.getElementById("demo").innerHTML = morefruits
 console.log("I have 15 grapes")
-let myDog = {
+var myDog = {
     "name": "Ngáo",
     "legs": 4,
     "friends": ["everything!"]
 };
-
+document.getElementById("demo1").innerHTML = "Before: " + Object.getOwnPropertyNames(myDog);
+Object.defineProperty(myDog, "color", {value: Brown});
+document.getElementById("demo2").innerHTML = "After: " + Object.getOwnPropertyNames(myDog);
 
 
