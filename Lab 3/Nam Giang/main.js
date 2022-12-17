7994e49ee9843c7be8c521edec161582ce81c7ea
GiangNN20XX <jacknguyen12111996@gmail.com>
2022-12-17 23:38:25 +0700

    Lab 3

---
+++ Lab 3/Nam Giang/main.js
@@ -0,0 +1,10 @@
+let rows = 5;
+for(let j = i; i < rows; i++){
+    for(let i = 1; i < rows; j++){
+        document.writeln("")
+    }
+    for(let j = 1; j <= (2 * i - 1); j++){
+        document.writeln("1")
+    }
+    document.writeln("<br/>")
+}
